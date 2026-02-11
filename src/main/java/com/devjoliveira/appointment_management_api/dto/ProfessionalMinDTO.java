fa26865ade4cacb1fd5dcc3c8c24c76e5508a2be
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2026-02-11 18:26:14 -0300

    Fix validation in professional dot

--- src/main/java/com/devjoliveira/appointment_management_api/dto/ProfessionalMinDTO.java
+++ src/main/java/com/devjoliveira/appointment_management_api/dto/ProfessionalMinDTO.java
@@ -2,10 +2,13 @@ package com.devjoliveira.appointment_management_api.dto;
 
 import com.devjoliveira.appointment_management_api.domain.Professional;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record ProfessionalMinDTO(
-                String name,
-                String specialty,
-                String email) {
+                @NotBlank String name,
+                @NotBlank String specialty,
+                @NotBlank @Email String email) {
 
         public ProfessionalMinDTO(Professional professional) {
                 this(professional.getName(), professional.getSpecialty(), professional.getEmail());
