0b7d677dd7242a3aae4b01541fcff42435d091bc
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2026-02-11 13:28:27 -0300

    Create project appointment management

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# Visão de Domínio 
+
+Em um salão de beleza, temos:
+
+* Customer 
+* Professional 
+* Service 
+* Appointment 
+* TimeSlot 
+* Payment 
+
+## O coração do sistema é:
+
+* Appointment
---
+++ src/test/java/com/devjoliveira/appointment_management_api/AppointmentManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjoliveira.appointment_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
