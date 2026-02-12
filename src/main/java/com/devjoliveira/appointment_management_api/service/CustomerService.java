da6521808a9733e96bd03fa85e6802ea87634cd2
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2026-02-11 23:05:36 -0300

    Fix customer service exceptions

--- src/main/java/com/devjoliveira/appointment_management_api/service/CustomerService.java
+++ src/main/java/com/devjoliveira/appointment_management_api/service/CustomerService.java
@@ -3,6 +3,7 @@ package com.devjoliveira.appointment_management_api.service;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,6 +11,7 @@ import com.devjoliveira.appointment_management_api.domain.Customer;
 import com.devjoliveira.appointment_management_api.dto.CustomerDTO;
 import com.devjoliveira.appointment_management_api.dto.CustomerMinDTO;
 import com.devjoliveira.appointment_management_api.repository.CustomerRepository;
+import com.devjoliveira.appointment_management_api.service.exceptions.ResourceNotFoundException;
 
 @Service
 public class CustomerService {
@@ -28,13 +30,13 @@ public class CustomerService {
   @Transactional(readOnly = true)
   public CustomerDTO findByEmail(String email) {
     return customerRepository.findByEmail(email).map(CustomerDTO::new).orElseThrow(
-        () -> new RuntimeException("Customer not found with email: " + email));
+        () -> new ResourceNotFoundException("Customer not found with email: " + email));
   }
 
   @Transactional
   public CustomerDTO save(CustomerMinDTO request) {
     customerRepository.findByEmail(request.email()).ifPresent(customer -> {
-      throw new RuntimeException("Customer with email " + customer.getEmail() + " already exists");
+      throw new DuplicateKeyException("Customer with email " + customer.getEmail() + " already exists");
     });
 
     Customer customer = new Customer();
@@ -50,11 +52,11 @@ public class CustomerService {
   public CustomerDTO change(UUID id, CustomerMinDTO request) {
 
     var fromDB = customerRepository.findById(id).orElseThrow(
-        () -> new RuntimeException("Customer with id " + id + " not found"));
+        () -> new ResourceNotFoundException("Customer with id " + id + " not found"));
 
     if (!fromDB.getEmail().equals(request.email())) {
       customerRepository.findByEmail(request.email()).ifPresent(customer -> {
-        throw new RuntimeException("Customer with email " + customer.getEmail() + " already exists");
+        throw new DuplicateKeyException("Customer with email " + customer.getEmail() + " already exists");
       });
     }
 
@@ -69,7 +71,7 @@ public class CustomerService {
   @Transactional
   public void delete(UUID id) {
     var fromDB = customerRepository.findById(id).orElseThrow(
-        () -> new RuntimeException("Customer with id " + id + " not found"));
+        () -> new ResourceNotFoundException("Customer with id " + id + " not found"));
 
     customerRepository.delete(fromDB);
   }
