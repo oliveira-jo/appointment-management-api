7a55f4ddb0cea8398c2ea10f0a59245d6b69851f
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2026-03-16 21:45:50 -0300

    Update: put metrics in dashboard too

--- frontend/src/app/features/admin/dashboard/dashboard.component.ts
+++ frontend/src/app/features/admin/dashboard/dashboard.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AppointmentService } from '../appointments/appointment.service';
+import { Metrics } from '../appointments/appointment-model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  metrics: Metrics = { todayAppointments: 0, weekAppointments: 0, todayRevenue: 0, totalAppointments: 0 }
+
+  constructor(private appointmentService: AppointmentService) { }
+
+  ngOnInit() {
+    this.loadMetrics();
+  }
+
+  loadMetrics() {
+    this.appointmentService
+      .getMetrics()
+      .subscribe(metrics => {
+        this.metrics = metrics;
+      })
+  }
 
 }
