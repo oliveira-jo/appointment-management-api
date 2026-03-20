6bde7289408a3c40272e075ea9c85b0d58b078ba
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2026-03-20 13:44:39 -0300

    Fix: dark mode and data presentation

--- frontend/src/app/layout/navbar/navbar.component.ts
+++ frontend/src/app/layout/navbar/navbar.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../core/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  isDarkMode: boolean = false;
   email: string | null = "";
 
   constructor(
@@ -24,6 +25,11 @@ export class NavbarComponent {
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark-mode', this.isDarkMode);
   }
 
 }
